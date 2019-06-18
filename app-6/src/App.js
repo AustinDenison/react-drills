84ae6ae03579f9417882554678b46a7923e212be
AustinDenison <austindenison1998@gmail.com>
2019-06-18 16:33:32 -0600

    app 6 done

--- app-6/src/App.js
+++ app-6/src/App.js
@@ -1,18 +1,41 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import Todo from './Components/Todo'
 
 class App extends Component {
+  constructor(){
+    super()
+
+    this.state = {
+        todoList:[],
+        item: ''
+
+      }
+    }
+    
+    handleNewTask(value){
+      this.setState({
+        item: value
+      })
+    }
+    
+    handleAdd(){
+      //this.state.todoList.push(this.state.item)
+      let copy = this.state.todoList.slice()
+      copy.push(this.state.item)
+      this.setState({item: '', todoList: copy})
+      
+}
+
+
   render() {
+    let mappedItems = this.state.todoList.map((value, index) => <Todo key={index} task={value}/>)
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>My to-do list:</h1>
+        <input value={this.state.item} placeholder='Enter new task' onChange={(e) => this.handleNewTask(e.target.value)} />
+        <button onClick={() => this.handleAdd()}>Add</button>
+        {mappedItems}
       </div>
     );
   }
